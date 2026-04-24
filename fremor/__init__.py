77185391551b13b2817a3306fd68905dea7aac9c
Ian L. <6273252+ilaflott@users.noreply.github.com>
2026-04-24 18:28:50 -0400

    Update __init__.py

--- fremor/__init__.py
+++ fremor/__init__.py
@@ -1,12 +1,13 @@
 """
-fremor: CMORization for FRE output
+``fremor``: CMORization for FRE output
 ==========================================
 
 This module provides routines which rewrite post-processed FRE/FMS model output in a community-driven, standardized way.
 This module relies heavily on PCMDI's CMOR module and it's ``python`` API. It is the core implementation for
 ``fremor run`` operations- mixing and matching GFDL's and FRE's conventions to CMOR's expectations, so that
 participation in model-intercomparison projects may be eased. For more usage details, see the project README.md, the
-FRE documentation, and PCMDI's CMOR module documentation available at https://cmor.llnl.gov/.
+FRE documentation at https://noaa-gfdl.readthedocs.io/projects/fre-cli/en/latest/, PCMDI's CMOR module documentation at 
+https://cmor.llnl.gov/, and of course ``fremor``'s documentation at https://fremorizer.readthedocs.io/en/stable/. 
 
 .. note:: this text is set by ``fremor/__init__.py``'s docstring. Importing ``fremor`` sets the version attribute, and sets 
           up a ``fre_logger``
