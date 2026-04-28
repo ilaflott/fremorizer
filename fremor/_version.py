23ec531435a12680bf04610fb78f9ae0c68ace9f
Ian L. <6273252+ilaflott@users.noreply.github.com>
2026-04-28 16:26:08 -0400

    Update _version.py

--- fremor/_version.py
+++ fremor/_version.py
@@ -3,5 +3,5 @@ version info for fremor, isolated to avoid importing heavy dependencies at build
 """
 
 import os
-version = os.getenv("GIT_DESCRIBE_TAG", "0.9.0.post")
+version = os.getenv("GIT_DESCRIBE_TAG", "0.9.1")
 __version__ = version
